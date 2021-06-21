35a30302f3efbdc7466306da3efdfdbc965d9345
nosfied <nosfied@gmail.com>
2021-06-21 14:33:49 -0300

    carrinho calculo de frete

--- site.php
+++ site.php
@@ -61,7 +61,8 @@ $app->get('/cart', function(){
     $page = New Page();
     $page->setTpl("cart", [
         'cart'=>$cart->getValues(),
-        'products'=>$cart->getProducts()
+        'products'=>$cart->getProducts(),
+        'error'=>Cart::getMsgError()
     ]);
 
 });
@@ -102,4 +103,13 @@ $app->get('/cart/:idproduct/remove', function($idproduct){
 
 });
 
- ?>
+$app->post('/cart/freight', function(){
+
+    $cart = Cart::getFromSession();
+    $cart->setFreight($_POST['zipcode']);
+    header("Location: /cart");
+    exit;
+
+});
+
+?>
