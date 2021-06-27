0ab69b15837c02f6ff05c81009586039c121ca46
nosfied <nosfied@gmail.com>
2021-06-27 10:19:20 -0300

    cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -304,6 +304,22 @@ class User extends Model
 
     }
 
+    public static function getErrorRegister()
+    {
+
+        $msg = (isset($_SESSION[User::ERROR_REGISTER]) && $_SESSION[User::ERROR_REGISTER])? $_SESSION[User::ERROR_REGISTER] : '';
+        User::clearErrorRegister();
+        return $msg;
+
+    }
+
+    public static function clearErrorRegister()
+    {
+
+        $_SESSION[User::ERROR_REGISTER] = NULL;
+
+    }
+
     public static function setErrorRegister($msg)
     {
 
@@ -320,5 +336,19 @@ class User extends Model
         ]);
 
     }
+
+    public static function checkLoginExist($login)
+    {
+
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin", [
+            ':deslogin' => $login
+        ]);
+
+        return (count($results) > 0);
+
+    }
+
+
 
 }
