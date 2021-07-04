ccf0766976614e86eff7bb8faee234f1b978f0da
nosfied <nosfied@gmail.com>
2021-07-04 14:47:44 -0300

    alterar a senha

--- site.php
+++ site.php
@@ -566,4 +566,75 @@ $app->get('/profile/orders/:idorder', function ($idorder){
 
 });
 
+$app->get('/profile/change-password', function (){
+
+    User::verifyLogin(false);
+    $page = new Page();
+    $page->setTpl('profile-change-password', [
+
+        'changePassError'=>User::getError(),
+        'changePassSuccess'=>User::getSuccess()
+
+    ]);
+
+});
+
+$app->post('/profile/change-password', function (){
+
+    User::verifyLogin(false);
+
+    if (!isset($_POST['current_pass']) || $_POST['current_pass'] === ''){
+
+
+        User::setError('Digite a Senha Atual.');
+        header('Location: /profile/change-password');
+        exit;
+
+    }
+
+    if (!isset($_POST['new_pass']) || $_POST['new_pass'] === ''){
+
+
+        User::setError('Digite a Nova Senha.');
+        header('Location: /profile/change-password');
+        exit;
+
+    }
+
+    if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === ''){
+
+
+        User::setError('Confirme a Nova Senha.');
+        header('Location: /profile/change-password');
+        exit;
+
+    }
+
+    if ($_POST['current_pass'] === $_POST['new_pass']){
+
+
+        User::setError('A sua nova senha deve ser diferente da atual.');
+        header('Location: /profile/change-password');
+        exit;
+
+    }
+
+    $user = User::getFromSession();
+
+    if (!password_verify($_POST['current_pass'], $user->getdespassword())){
+
+        User::setError('A senha está inválida.');
+        header('Location: /profile/change-password');
+        exit;
+
+    }
+
+    $user->setPassword($_POST['new_pass']);
+    $user->update();
+    User::setSuccess('Senha alterada com sucesso');
+    header('Location: /profile/change-password');
+    exit;
+
+});
+
 ?>
